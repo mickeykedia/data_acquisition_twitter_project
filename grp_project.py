cc6f359d05981c6ba43270a8819a713a8c836c85
sakshi bhargava <sakshibhargava024@gmail.com>
2015-10-01 22:53:29 -0700

    updated with all tweet fields

--- grp_project.py
+++ grp_project.py
@@ -1,38 +1,6 @@
-
-
-# No. of followers, No. of followed, No. of tweets
-# from rauth import OAuth1Service
-# twitter = OAuth1Service(
-#     name='twitter',
-#     consumer_key='pnE9kjOJOhAO3WTUMJ4rLRe1W',
-#     consumer_secret='e7pEcYtCBkr3jjalFjlVNakcjZkn4sWGD5pKLDASA3zAnLnRNz',
-#     request_token_url='https://api.twitter.com/oauth/request_token',
-#     access_token_url='https://api.twitter.com/oauth/access_token',
-#     authorize_url='https://api.twitter.com/oauth/authorize',
-#     base_url='https://api.twitter.com/1.1/')
-#
-#
-# request_token, request_token_secret = twitter.get_request_token()
-#
-# authorize_url = twitter.get_authorize_url(request_token)
-# pin = 7635292
-# session = twitter.get_auth_session(request_token,
-#                                    request_token_secret,
-#                                    method='POST',
-#                                    data={'oauth_verifier': pin})
-#
-# params = {'screen_name': 'RevRunWisdom',  # User to pull Tweets from
-#           'include_rts': 1,         # Include retweets
-#           'count': 100}              # 10 tweets
-#
-# r = session.get('statuses/user_timeline.json', params=params)
-# for i, tweet in enumerate(r.json(), 1):
-#     handle = tweet['user']['screen_name'].encode('utf-8')
-#     text = tweet['text'].encode('utf-8')
-#     print '{0}. @{1} - {2}'.format(i, handle, text)
-
-
 import tweepy
+import re
+
 
 ckey = 'pnE9kjOJOhAO3WTUMJ4rLRe1W'
 csecret = 'e7pEcYtCBkr3jjalFjlVNakcjZkn4sWGD5pKLDASA3zAnLnRNz'
@@ -46,34 +14,52 @@ api = tweepy.API(auth)
 
 user1 = api.get_user("BillGates")
 
-tweets = api.user_timeline(screen_name="BillGates", include_rts=True, count=300)
+tweets = api.user_timeline(screen_name="BillGates", include_rts=True, count=200)
 
-tweets_for_csv = [['BillGates',tweet.id_str, tweet.created_at, tweet.text.encode("utf-8")] for tweet in tweets]
-for tweet in tweets:
-    print len(tweets)
-    print tweet.id,  tweet.text.encode("utf-8"), tweet.retweet_count
+# tweets_for_csv = [['BillGates',tweet.id_str, tweet.created_at, tweet.text.encode("utf-8")] for tweet in tweets]
+
 
 
 for tweet in tweets:
     print ("ID:", tweet.id)
+    print("ID_str:", tweet.id_str)
     print ("User ID:", tweet.user.id)
-    print ("Text:", tweet.text)
+    print ("Text:", re.sub(r'[^\x00-\x7F]+','', str(tweet.text.encode("utf-8"))))
     print ("Created:", tweet.created_at)
-    print ("Geo:", tweet.geo)
-    print ("Contributors:", tweet.contributors)
+    # print ("Geo:", tweet.geo)
+    # print ("Contributors:", tweet.contributors)
     print ("Coordinates:", tweet.coordinates)
-    print ("Favorited:", tweet.favorited)
-    print ("In reply to screen name:", tweet.in_reply_to_screen_name)
-    print ("In reply to status ID:", tweet.in_reply_to_status_id)
-    print ("In reply to status ID str:", tweet.in_reply_to_status_id_str)
-    print ("In reply to user ID:", tweet.in_reply_to_user_id)
-    print ("In reply to user ID str:", tweet.in_reply_to_user_id_str)
+    print ("Favorite Count:", tweet.favorite_count)
     print ("Place:", tweet.place)
-    print ("Retweeted:", tweet.retweeted)
+    # whether this Tweet has been retweeted by the authenticating user.
+    # print ("Retweeted:", tweet.retweeted)
+    # print ("entities:", tweet.entities)
+    # print tweet.entities
+    if tweet.entities['hashtags'] <> []:
+        print("has_hashtag", 1)
+    else:
+      print("has_hashtag", 0)
+
+    if tweet.entities['urls'] <> []:
+        print("has_url", 1)
+    else:
+      print("has_url", 0)
+
+    if 'media' in tweet.entities:
+        print("has_media", 1)
+        if tweet.entities['media'][0]['type'] == 'photo':
+            print("has_photo", 1)
+            print("has_video", 0)
+        else:
+            print("has_video", 1)
+            print("has_photo", 0)
+    else:
+      print("has_media", 0)
+
     print ("Retweet count:", tweet.retweet_count)
     print ("Source:", tweet.source)
-    print ("Truncated:", tweet.truncated)
 
+exit()
 
 def profile_info(user):
     user = api.get_user(user)
